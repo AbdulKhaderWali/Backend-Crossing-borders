fa7fdf810da91f203ea7b5dcc4dc963f2660485d
Abdul Khader Wali <abdul.khader5166@gmail.com>
2023-05-15 14:00:07 +0530

    environment variables added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./routes/routes.js');
+require('dotenv').config();
 const cors = require('cors');
-
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
 
 const app = express();
 
@@ -11,13 +13,13 @@ app.use(express.urlencoded());
 app.use(cors());
 app.use('/', route);
 
-mongoose.connect("mongodb+srv://collectionhomework:GNh6KXEVKLbkv78R@cluster0.lymgtlv.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
 
-app.listen(8000,() => {
-    console.log("Server is running on port 8000");
+app.listen(PORT,() => {
+    console.log(`SERVER IS RUNNING ON ${PORT}`);
 })
